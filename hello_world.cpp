a71efabdb7c5ec58067c5c9ecbece620dc17d7fd
qur1l <tuzyakaa@mail.ru>
2024-05-25 16:04:36 -0400

    Part 1, step 6-9

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
-int main(){cout << "Hello World!"; return 0;}
+int main() {
+    string name; cin >> name;
+    cout << "Hello world from " << name;
+    return 0;
+}
