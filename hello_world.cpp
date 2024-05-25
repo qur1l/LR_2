e2e58be62573908e33ba55d26758fda2b8b20124
qur1l <144834356+qur1l@users.noreply.github.com>
2024-05-25 23:17:11 +0300

    Part 3, step 4 - conflict

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,8 @@
 
 
 int main() {
-    std::string name; std::cin >> name; // Gets users name
-    std::cout << "Hello world from " << name;
+    std::string name;
+    std::cin >> name; // Gets users name
+    std::cout << "Hello World from " << name;
     return 0;
 }
