75e1738d2d41bfcd19cdd1bdd7d6da2c8c0d0562
qur1l <tuzyakaa@mail.ru>
2024-05-25 16:09:40 -0400

    Part 2, step 6-7

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 
 
 int main() {
-    std::string name; std::cin >> name;
+    std::string name; std::cin >> name; // Gets users name
     std::cout << "Hello world from " << name;
     return 0;
 }
